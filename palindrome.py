8132935da1ef452918e4056dd3c0a0bd5097ca14
Reed Gotcher <brgotcher@gmail.com>
2021-06-02 19:26:29 -0500

    adjust everything

--- palindrome.py
+++ palindrome.py
@@ -8,11 +8,11 @@ def rec_checkPalindrome_1(string, m, n):
     # m starts at 0 index and works toward the end
     # if m has reached the end, return n
     if m == (len(string)-1):
-        return n
+        return n-1
     # n starts at the end of the string and works toward the beginning
     # if n reaches the beginning, return m
-    if not n:
-        return m
+    if n == 1:
+        return (len(string)-1) - m
 
     # if the elements at the two current indices match,
     # they do not need to be removed.
@@ -23,12 +23,9 @@ def rec_checkPalindrome_1(string, m, n):
     # if the elements at the two current indices do not match,
     # one of them needs to be removed.  Try both and use whichever ends
     # up with the lowest count
-    move_m = rec_checkPalindrome_1(string, m+1, n)
-    move_n = rec_checkPalindrome_1(string, m, n-1)
-    result = 1 + min(move_m, move_n)
-    # result = 1 + min(rec_checkPalindrome_1(string, m+1, n), rec_checkPalindrome_1(string, m, n-1))
+    result = 1 + min(rec_checkPalindrome_1(string, m+1, n), rec_checkPalindrome_1(string, m, n-1))
     return result
 
-string = "abcdcbfaf"
+string = "abcxdcbfaf"
 k = 2
 print(checkPalindrome_1(string, k))
