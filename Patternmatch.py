9bb564294b05d069f7bb39d6805ad863322bf789
brgotcher <brgotcher@gmail.com>
2021-06-04 06:53:43 -0500

    continue patternmatch

--- Patternmatch.py
+++ Patternmatch.py
@@ -1,6 +1,20 @@
 def patternmatch(string, p):
-    cache = [[0 for i in range(len(p)+1)] for j in range(len(string)+1)]
-    m = 0
-    n = 0
-    while m < len(p) and n < len(string):
-        
+    plength = len(p)
+    slength = len(string)
+    cache = [[0 for i in range(plength)] for j in range(slength)]
+    for i in range(slength):
+        for j in range(plength):
+            if p[j] == '*':
+                if i == 0 or cache[i-1][j] or cache[i][j-1]:
+                    cache[i][j] = 1
+            elif p[j] == '?':
+                if j == 0:
+                    if i == 0:
+                        cache[i][j] == 1
+
+                elif i > 1:
+                    cache[i][j] = cache[i-1][j-1]
+                elif p[j-1] == '*':
+                    cache[i][j] = cache[i-1][j]
+            elif p[j] == i[j]:
+                cache[i][j] = cache[i-1][j-1]
